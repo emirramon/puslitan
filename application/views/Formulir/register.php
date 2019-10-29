73a9996a3794730f7cbf8a48e688bfa73f670e92
emirramon <emirramoon@gmail.com>
2019-10-29 10:26:41 +0700

    test99

--- application/views/Formulir/register.php
+++ application/views/Formulir/register.php
@@ -176,30 +176,3 @@
         </div>
     </div>
 </div>
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-<script>
-    $(document).ready(function() {
-        var url = 'http://localhost/puslitan/Formulir/register';
-
-        $('#fakultas').change(function() {
-            var id = $(this).val();
-            // alert(id)
-            var data = {
-                id: id
-            };
-            $.post(url, data, function(r) {
-                var html = '';
-                for (let index = 0; index < r.length; index++) {
-                    html += '<option value="' + r[index].idjurusan + '">' + r[index].namajurusan + '</option>';
-
-                }
-                $('#jurusan').html(html);
-
-            }, 'JSON');
-        });
-    });
-</script>
